11c4d81561bc7e8a41cba633730779a9752748c5
Peter Casey <pcasey@cocc.edu>
2019-11-07 12:33:04 -0800

    Added a few lines to main()

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ import binheap
 def random_list(low=0, high=100, count=20):
     return [randint(low, high) for _ in range(count)]
 
+
 def main():
     nums = random_list(count=10)
-    print(nums)
+    print("List:", nums)
+    heap = BinHeap(False)
+    heap.build_heap(nums)
+    print("Heap:", heap)
+    heap.sort()
+    print("Sorted:", heap)
+
 
 if __name__ == "__main__":
     main()
